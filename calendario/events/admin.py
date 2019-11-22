0eaf112dbc6addffb32a3f47436c0711263c6e88
Melba Consuelos <melbaconsuelos@hotmail.com>
2019-11-21 23:02:29 -0600

    scheduling of events and mini calendar

--- calendario/events/admin.py
+++ calendario/events/admin.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
+ 
 from django.contrib import admin
+from django.db import models
+from events.models import Event 
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['day', 'start_time', 'end_time', 'notes']
+
